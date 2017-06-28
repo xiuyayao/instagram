1b3daf59a4fd8e5b62017487137859e329c56898
Xiuya Yao <xiuyayao@xiuyayao.dhcp.thefacebook.com>
2017-06-27 17:03:36 -0700

    Sign in view set up

---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,58 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by Xiuya Yao on 6/27/17.
+//  Copyright © 2017 Xiuya Yao. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    let loginAlertController = UIAlertController(title: "Invalid Input", message: "Please valid username AND password", preferredStyle: .alert)
+    
+    @IBOutlet weak var usernameLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+    
+    
+    @IBAction func loginUser(_ sender: UIButton) {
+        if usernameLabel.text!.isEmpty || passwordLabel.text!.isEmpty {
+            self.present(self.loginAlertController, animated: true)
+        }
+        
+        let username = usernameLabel.text ?? ""
+        let password = passwordLabel.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+            } else {
+                print("User logged in successfully")
+                // display view controller that needs to shown after successful login
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            // handle response here.
+        }
+        // add the OK action to the alert controller
+        loginAlertController.addAction(OKAction)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Instagram/SignUpViewController.swift
@@ -0,0 +1,82 @@
+//
+//  SignUpViewController.swift
+//  Instagram
+//
+//  Created by Xiuya Yao on 6/27/17.
+//  Copyright © 2017 Xiuya Yao. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SignUpViewController: UIViewController {
+    
+    let signUpAlertController = UIAlertController(title: "Invalid Input", message: "Please enter valid credentials", preferredStyle: .alert)
+    
+    @IBOutlet weak var emailLabel: UITextField!
+    @IBOutlet weak var usernameLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+    
+    @IBAction func registerUser(_ sender: UIButton) {
+        if emailLabel.text!.isEmpty || usernameLabel.text!.isEmpty || passwordLabel.text!.isEmpty {
+            self.present(self.signUpAlertController, animated: true)
+        }
+        
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = usernameLabel.text
+        newUser.email = emailLabel.text
+        newUser.password = passwordLabel.text
+        
+        // call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                self.present(self.signUpAlertController, animated: true)
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                // manually segue to logged in view
+                // dismiss modal view
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func cancelRegisterAction(_ sender: UIButton) {
+        // dismiss modal view
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            // handle response here.
+        }
+        // add the OK action to the alert controller
+        signUpAlertController.addAction(OKAction)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
